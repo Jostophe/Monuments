94fe18d8b08a2e246367507205819fbdfe677631
Slawomir STAWORKO <sstaworko@A2419-01.peda.pdb.univ-lille3.fr>
2017-04-06 13:46:04 +0200

    ajout de fichier de test

---
+++ sqltest.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+   <meta charset="utf-8"/>
+   <title>SQLITE TEST</title>
+</head>
+<body>
+<?php
+    $db = new SQLite3('sql.db');
+    $db->exec('INSERT INTO R VALUES (3)');
+
+
+    $results = $db->query('SELECT A+A FROM R');
+    echo "<ul>"; 
+    while ($row = $results->fetchArray()) {
+        echo "<li>";
+        echo $row[0];
+        echo "</li>\n";
+    }
+    echo "</ul>\n";
+?>
+</body>
+</html>
